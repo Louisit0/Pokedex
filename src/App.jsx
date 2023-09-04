4603484df0ab40d81039c84c25d8c03d4327b4f0
Luis <lrangarita7@gmail.com>
2023-09-04 18:42:09 -0300

    app "layout de la app"

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Buscador from "./components/buscador";
+import PokeInfo from "./components/PokeInfo";
+import ListaPokemon from "./components/ListaPokemon";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-full bg-slate-50 flex flex-col px-40">
+      <div className="flex flex-row gap-4 mt-8">
+        <div className="w-full">
+          <Buscador />
+          <div className="mt-16">
+            <ListaPokemon />
+          </div>
+        </div>
+        <div className="w-1/2">
+          <PokeInfo />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;

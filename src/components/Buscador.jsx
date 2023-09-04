a05c1245e7c04fee191764f5e163b9cab41eef6f
Luis <lrangarita7@gmail.com>
2023-09-04 18:42:24 -0300

    componente buscador

---
+++ src/components/Buscador.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Buscador = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Busca tu pokemon!"
+        className="w-full p-4 bg-white shadow-sm rounded-xl"
+      />
+    </div>
+  );
+};
+
+export default Buscador;

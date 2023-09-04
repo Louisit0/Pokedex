c96acc57bdfba16f2e3f3a22bd861971aadfbddb
Luis <lrangarita7@gmail.com>
2023-09-04 18:44:02 -0300

    array de colores por tipo

---
+++ src/utilities/ColoresTipo.jsx
@@ -0,0 +1,22 @@
+const ColoresTipo = {
+  normal: "bg-yellow-300",
+  fire: "bg-red-400",
+  water: "bg-blue-400",
+  grass: "bg-green-400",
+  flying: "bg-indigo-400",
+  fighting: "bg-red-700",
+  poison: "bg-purple-400",
+  electric: "bg-yellow-400",
+  ground: "bg-yellow-800",
+  rock: "bg-yellow-600",
+  psychic: "bg-pink-400",
+  ice: "bg-blue-200",
+  bug: "bg-green-300",
+  ghost: "bg-indigo-700",
+  steel: "bg-gray-400",
+  dragon: "bg-indigo-900",
+  dark: "bg-gray-700",
+  fairy: "bg-pink-300",
+};
+
+export default ColoresTipo;

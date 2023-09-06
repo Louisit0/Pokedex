cb270cfb939142eaf0b356411eaf69cd6016818e
Louisit0 <lrangarita7@gmail.com>
2023-09-06 00:39:11 -0300

    props pokemonInfo

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ import ListaPokemon from "./components/ListaPokemon";
 import "./App.css";
 
 function App() {
+  const [pokemonInfo, setPokemonInfo] = useState({});
+
   return (
     <div className="h-screen w-full bg-slate-50 flex flex-col px-40">
       <div className="flex flex-row gap-4 mt-8">
         <div className="w-full">
           <Buscador />
           <div className="mt-16">
-            <ListaPokemon />
+            <ListaPokemon
+              pokemonInfo={pokemonInfo}
+              setPokemonInfo={setPokemonInfo}
+            />
           </div>
         </div>
         <div className="w-1/2">
-          <PokeInfo />
+          <PokeInfo pokemonInfo={pokemonInfo} />
         </div>
       </div>
     </div>

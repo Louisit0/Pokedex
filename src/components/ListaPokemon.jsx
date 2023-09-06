1030cb745c73f571a0d6669f390d9a0f87724d14
Louisit0 <lrangarita7@gmail.com>
2023-09-06 00:42:30 -0300

    ahora puedes ver el pokemon al clikearlo, aparece con como gif. Pokemon info ahora es un objeto con varios atributos para ser mostrados en el componente pokeinfo.

--- src/components/ListaPokemon.jsx
+++ src/components/ListaPokemon.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import ColoresTipo from "../utilities/ColoresTipo";
 
-const ListaPokemon = () => {
+const ListaPokemon = ({ pokemonInfo, setPokemonInfo }) => {
   const [pokemonData, setPokemonData] = useState([]);
   const [pokemonTypes, setPokemonTypes] = useState({});
 
@@ -40,34 +40,52 @@ const ListaPokemon = () => {
     fetchPokemonData();
   }, []);
 
+  const mostrarDetalles = (pokemon, index) => {
+    setPokemonInfo({
+      id: index + 1,
+      name: pokemon.name,
+      types: pokemonTypes[pokemon.name] || [],
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${
+        index + 1
+      }.gif`,
+    });
+    console.log("pokemonInfo: ", pokemon);
+  };
+
   return (
     <div className="h-screen">
-      <div className="grid grid-cols-3 gap-5">
+      <div className="grid grid-cols-4 gap-5">
         {pokemonData.map((pokemon, index) => (
-          <div key={index} className="bg-white p-4 rounded-xl text-center">
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                index + 1
-              }.png`}
-              alt={pokemon.name}
-              className="mx-auto"
-            />
-            <p className="font-bold text-gray-400">#{index + 1}</p>
-            <h3 className="font-bold mb-2">{pokemon.name}</h3>
-            <div className="flex flex-row justify-evenly">
-              {pokemonTypes[pokemon.name] &&
-                pokemonTypes[pokemon.name].map((type, typeIndex) => (
-                  <span
-                    key={typeIndex}
-                    className={`px-4 py-1 self-center rounded-lg font-bold capitalize text-sm ${
-                      ColoresTipo[type] || "bg-gray-500"
-                    }`}
-                  >
-                    {type}
-                  </span>
-                ))}
+          <a
+            key={index}
+            className="cursor-pointer"
+            onClick={() => mostrarDetalles(pokemon, index)}
+          >
+            <div className="bg-white p-4 rounded-xl text-center">
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                  index + 1
+                }.png`}
+                alt={pokemon.name}
+                className="mx-auto"
+              />
+              <p className="font-bold text-gray-400">#{index + 1}</p>
+              <h3 className="font-bold mb-2">{pokemon.name}</h3>
+              <div className="flex flex-row justify-evenly">
+                {pokemonTypes[pokemon.name] &&
+                  pokemonTypes[pokemon.name].map((type, typeIndex) => (
+                    <span
+                      key={typeIndex}
+                      className={`px-4 py-1 self-center rounded-lg font-bold capitalize text-sm ${
+                        ColoresTipo[type] || "bg-gray-500"
+                      }`}
+                    >
+                      {type}
+                    </span>
+                  ))}
+              </div>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </div>

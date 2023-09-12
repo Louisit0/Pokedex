e74bed65f91579ccd28c39c5cef75be91d4e246d
Luis <lrangarita7@gmail.com>
2023-09-12 01:22:02 -0300

    pokemondetail new attribute: species: flavorText and some responsive

--- src/components/ListaPokemon.jsx
+++ src/components/ListaPokemon.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import ColoresTipo from "../utilities/ColoresTipo";
+import PokeInfo from "./PokeInfo";
 
 const ListaPokemon = ({ pokemonInfo, setPokemonInfo }) => {
   const [pokemonData, setPokemonData] = useState([]);
@@ -25,20 +26,44 @@ const ListaPokemon = ({ pokemonInfo, setPokemonInfo }) => {
           if (response.ok) {
             const pokemonDetails = await response.json();
             console.log(pokemonDetails);
-            // Almacena los detalles del Pokémon en una variable local
-            const pokemonDetail = {
-              name: pokemonDetails.name,
-              types: pokemonDetails.types.map((type) => type.type.name),
-              height: pokemonDetails.height,
-              weight: pokemonDetails.weight,
-              stats: pokemonDetails.stats,
-              abilities: pokemonDetails.abilities,
-            };
-            // console.log(pokemonDetail);
-            setPokemonTypes((prevTypes) => ({
-              ...prevTypes,
-              [pokemon.name]: pokemonDetail,
-            }));
+
+            // Obtén la URL de la especie
+            const speciesUrl = pokemonDetails.species.url;
+
+            // Realiza una solicitud para obtener los detalles de la especie
+            const speciesResponse = await fetch(speciesUrl);
+            if (speciesResponse.ok) {
+              const speciesData = await speciesResponse.json();
+
+              // Encuentra la entrada de texto en el idioma "en" (inglés)
+              const flavorTextEntry = speciesData.flavor_text_entries.find(
+                (entry) => entry.language.name === "en"
+              );
+
+              if (flavorTextEntry) {
+                const flavorText = flavorTextEntry.flavor_text;
+                console.log(flavorText);
+
+                // Almacena los detalles del Pokémon en una variable local
+                const pokemonDetail = {
+                  name: pokemonDetails.name,
+                  types: pokemonDetails.types.map((type) => type.type.name),
+                  height: pokemonDetails.height,
+                  weight: pokemonDetails.weight,
+                  stats: pokemonDetails.stats,
+                  abilities: pokemonDetails.abilities,
+                  species: {
+                    name: speciesData.name,
+                    url: speciesUrl,
+                    flavorText: flavorText,
+                  },
+                };
+                setPokemonTypes((prevTypes) => ({
+                  ...prevTypes,
+                  [pokemon.name]: pokemonDetail,
+                }));
+              }
+            }
           }
         });
       } catch (error) {
@@ -63,6 +88,7 @@ const ListaPokemon = ({ pokemonInfo, setPokemonInfo }) => {
       weight: pokemonDetail.weight || 0,
       stats: pokemonDetail.stats,
       abilities: pokemonDetail.abilities,
+      species: pokemonDetail.species,
     });
     console.log("pokemonInfo: ", pokemon);
   };
@@ -73,48 +99,54 @@ const ListaPokemon = ({ pokemonInfo, setPokemonInfo }) => {
   };
 
   return (
-    <div className="h-screen">
-      <div className="grid grid-cols-4 gap-5">
-        {pokemonData.map((pokemon, index) => (
-          <a
-            key={index}
-            className="cursor-pointer"
-            onClick={() => mostrarDetalles(pokemon, index)}
-          >
-            <div className="bg-white p-4 rounded-xl text-center">
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                  index + 1
-                }.png`}
-                alt={pokemon.name}
-                className="mx-auto"
-              />
-              <p className="font-bold text-gray-400">#{index + 1}</p>
-              <h3 className="font-bold mb-2">{pokemon.name}</h3>
-              <div className="flex flex-row justify-evenly">
-                {pokemonTypes[pokemon.name] &&
-                  pokemonTypes[pokemon.name].types.map((type, typeIndex) => (
-                    <span
-                      key={typeIndex}
-                      className={`px-4 py-1 self-center rounded-lg font-bold capitalize text-sm ${
-                        ColoresTipo[type] || "bg-gray-500"
-                      }`}
-                    >
-                      {type}
-                    </span>
-                  ))}
+    <div className="h-screen flex flex-row w-full gap-5">
+      <div className="w-full md:w-2/3">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-5 bg-slate-100">
+          {pokemonData.map((pokemon, index) => (
+            <a
+              key={index}
+              className="cursor-pointer"
+              onClick={() => mostrarDetalles(pokemon, index)}
+            >
+              <div className="bg-white p-4 rounded-xl text-center">
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                    index + 1
+                  }.png`}
+                  alt={pokemon.name}
+                  className="mx-auto"
+                />
+                <p className="font-bold text-gray-400">#{index + 1}</p>
+                <h3 className="font-bold mb-2">{pokemon.name}</h3>
+                <div className="flex flex-row justify-center gap-2">
+                  {pokemonTypes[pokemon.name] &&
+                    pokemonTypes[pokemon.name].types.map((type, typeIndex) => (
+                      <span
+                        key={typeIndex}
+                        className={`px-4 py-1 self-center rounded-lg font-bold capitalize text-sm ${
+                          ColoresTipo[type] || "bg-gray-500"
+                        }`}
+                      >
+                        {type}
+                      </span>
+                    ))}
+                </div>
               </div>
-            </div>
-          </a>
-        ))}
+            </a>
+          ))}
+        </div>
+        <div className="flex justify-center">
+          <button
+            onClick={cargarMas}
+            className="py-2 px-8 bg-blue-900 font-bold rounded-2xl text-white my-10 w-1/2"
+          >
+            Cargar más
+          </button>
+        </div>
       </div>
       {/* Futuro: que cargue a medida que scrolleas */}
-      <button
-        onClick={cargarMas}
-        className="py-2 px-8 bg-black text-white my-10 mx-auto w-full"
-      >
-        Cargar más
-      </button>
+
+      <PokeInfo pokemonInfo={pokemonInfo} />
     </div>
   );
 };
